77a91db3320e4a40c7e01ed0e314bf7332e5950c
nitin-yadav-dev <nitishyadavdev@gmail.com>
2020-10-13 11:19:45 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from "@angular/material/sidenav";
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatIconModule, MatToolbarModule, MatSidenavModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
